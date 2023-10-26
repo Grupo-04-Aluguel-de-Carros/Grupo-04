9e320759abb84672817558d62415ec29c0da4e1a
GustavonBrito <gustavoserafio@gmail.com>
2023-10-26 20:57:05 -0300

    Alteração na tabela carro

--- src/modules/brand/brand.repository.js
+++ src/modules/brand/brand.repository.js
@@ -1,7 +1,7 @@
-import { db } from '../../config/db.js';
+// import { db } from '../../config/db.js';
 
-export const createBrandRepo = async name => {
-  try {
-    return db.user.name;
-  } catch (error) {}
-};
+// export const createBrandRepo = async (name) => {
+//   try {
+//     return await db.
+//   } catch (error) {}
+// };
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,5 @@
 import { Router } from 'express';
-const brand = require('./brand');
 
 const routes = Router();
-
-routes.use('/brand', brand);
 
 export default routes;
