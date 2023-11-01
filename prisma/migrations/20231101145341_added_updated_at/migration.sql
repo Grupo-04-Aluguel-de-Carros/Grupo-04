436ceb2fb71461f779d4d0658eb8bd7cdb1bb46e
GustavonBrito <gustavoserafio@gmail.com>
2023-11-01 11:54:03 -0300

    added updated at

--- prisma/migrations/20231101145341_added_updated_at/migration.sql
+++ prisma/migrations/20231101145341_added_updated_at/migration.sql
@@ -3,11 +3,15 @@
 
   - Added the required column `updatedAt` to the `Address` table without a default value. This is not possible if the table is not empty.
   - Added the required column `updatedAt` to the `Brand` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updatedAt` to the `Car` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updatedAt` to the `Category` table without a default value. This is not possible if the table is not empty.
   - Added the required column `updatedAt` to the `Store` table without a default value. This is not possible if the table is not empty.
 
 */
+-- DropForeignKey
+ALTER TABLE "Car" DROP CONSTRAINT "Car_brandId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Car" DROP CONSTRAINT "Car_categoryId_fkey";
+
 -- AlterTable
 ALTER TABLE "Address" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
@@ -17,13 +21,11 @@ ALTER TABLE "Brand" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CUR
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
 
 -- AlterTable
-ALTER TABLE "Car" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
-
--- AlterTable
-ALTER TABLE "Category" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
-
--- AlterTable
 ALTER TABLE "Store" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Car" ADD CONSTRAINT "Car_brandId_fkey" FOREIGN KEY ("brandId") REFERENCES "Brand"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Car" ADD CONSTRAINT "Car_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
