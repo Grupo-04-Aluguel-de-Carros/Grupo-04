f6de43a5fd961cf91e5c2e8034b53f12c72f29e6
GustavonBrito <gustavoserafio@gmail.com>
2023-11-09 18:35:48 +0000

    Crud em andamento

--- src/modules/feature/Repository/createFeatureRepo.js
+++ src/modules/feature/Repository/createFeatureRepo.js
@@ -2,7 +2,8 @@ import { db } from "../../../config/db.js"
 
 export const createFeatureRepo = async (massageSystem, shielding, sunRoof, gearLever, selfDriving, zeroToHundred, displacement, power, velocity) => {
     try {
-        const feature = db.
+       
+
     } catch (error) {
         
     }
