2a1fce2013ce22f0a41bfcdf5df1a72312c2eccd
caioNunesS <caiogrb@gmail.com>
2023-11-17 10:32:24 -0300

    fix

---
+++ src/modules/car/Dto/unit-tests/createCarSchema.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import { createCarSchema } from '../createCarSchema.js';
+
+describe('create car schema', () => {
+  describe('when a valid body', () => {
+    it('should validates a body with all fields', () => {
+      const validInput = {
+        name: 'Gol',
+        color,
+      };
+    });
+  });
+});
