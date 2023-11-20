dbdb5436bd698d48de196fe125b3f580d5a7b793
GustavonBrito <gustavoserafio@gmail.com>
2023-11-20 19:33:24 -0300

    Fix feature

--- src/modules/feature/Service/createFeatureService.js
+++ src/modules/feature/Service/createFeatureService.js
@@ -1,7 +1,7 @@
 import { HttpStatusCode } from 'axios';
 import { createFeatureRepo } from '../Repository/createFeatureRepo.js';
 import { findFeatureByName } from '../Repository/findFeatureByName.js';
-import { findArrayCarIdRepo } from '../Repository/findArrayCarIdRepo.js';
+import { findArrayCarIdRepo } from '../../car/Repository/findArrayCarIdRepo.js';
 
 export const createFeatureService = async (featureObject, carId) => {
   try {
@@ -10,9 +10,9 @@ export const createFeatureService = async (featureObject, carId) => {
     const existentCarId = await findArrayCarIdRepo();
     if (existentCarId !== null) {
       for (let i = 0; i < existentCarId.length; i++) {
-        carIdFromExistentCarId = existentCarId[i].cars[0];
+        carIdFromExistentCarId = existentCarId[i].id;
         for (let i = 0; i < carId.length; i++) {
-          const found = carIdFromExistentCarId.carId.matchAll(carId[i]);
+          const found = carIdFromExistentCarId.matchAll(carId[i]);
           for (const match of found) {
             carsEncoutered.push(match[0]);
           }
