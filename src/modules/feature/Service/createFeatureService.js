b446d9b08df1780bb728dd5c1e859946321830cb
GustavonBrito <gustavoserafio@gmail.com>
2023-11-20 16:13:54 -0300

    Car id sendo verificado antes de criar

--- src/modules/feature/Service/createFeatureService.js
+++ src/modules/feature/Service/createFeatureService.js
@@ -1,30 +1,29 @@
 import { HttpStatusCode } from 'axios';
 import { createFeatureRepo } from '../Repository/createFeatureRepo.js';
 import { findFeatureByName } from '../Repository/findFeatureByName.js';
-import { findArrayCarById } from '../../car/Repository/findArrayCarById.js';
+import { findArrayCarIdRepo } from '../Repository/findArrayCarIdRepo.js';
 
 export const createFeatureService = async (featureObject, carId) => {
   try {
     let carsEncoutered = [];
-    const existentCarId = await findArrayCarById()
+    let carIdFromExistentCarId;
+    const existentCarId = await findArrayCarIdRepo();
     if (existentCarId !== null) {
-      let carIdFromExistentCarId;
-      for (let i = 0; i < existentCarId[0].cars.length; i++) {
-        carIdFromExistentCarId = existentCarId[0].cars[i];
+      for (let i = 0; i < existentCarId.length; i++) {
+        carIdFromExistentCarId = existentCarId[i].cars[0];
         for (let i = 0; i < carId.length; i++) {
-          const found = carIdFromExistentCarId.carId.matchAll(carId[i]); 
+          const found = carIdFromExistentCarId.carId.matchAll(carId[i]);
           for (const match of found) {
-            carsEncoutered.push(match[0])
+            carsEncoutered.push(match[0]);
           }
         }
       }
     }
-    
     if (carsEncoutered.length !== carId.length) {
-      throw{
+      throw {
         message: `Apenas os carros de ids ${carsEncoutered} existem, verifique os outros`,
-        status: HttpStatusCode.BadRequest
-      }
+        status: HttpStatusCode.BadRequest,
+      };
     }
 
     const featureByNameFromRepo = await findFeatureByName(featureObject.name);
